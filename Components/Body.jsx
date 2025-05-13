bd2e2944af38cdb21f8b1cddfa22805020690b68
Yaswanth <palloliyaswanth@gmail.com>
2025-05-14 00:11:53 +0530

    Sign-in&sign-up && Redux Setup

---
+++ Components/Body.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+
+import { useContext } from "react";
+
+import { BrowserRouter, Route, Routes, Link, NavLink } from "react-router-dom";
+
+import Profile from "../Components/Profile.jsx";
+
+import About from "../Components/About.jsx";
+import Nav from "../Components/Nav.jsx";
+
+
+
+import { createContext } from "react";
+import SignPage from "../Components/SignPage.jsx";
+
+const ThemeContext = createContext();
+
+const Product = () => {
+  const theme = useContext(ThemeContext);
+
+  return (
+    <div>
+      <h1 style={{ color: theme === "light" ? "blue" : "red" }}>
+        This is product
+      </h1>
+    </div>
+  );
+};
+
+const Category = () => {
+  return (
+    <div>
+      <h1 className="text-center">This is Category</h1>
+      <Product />
+    </div>
+  );
+};
+
+const Home = () => {
+  const theme = "light";
+  return (
+    <div className="home">
+    
+
+      <ThemeContext.Provider value={theme}>
+        <Category />
+      </ThemeContext.Provider>
+    </div>
+  );
+};
+
+function Body() {
+
+  
+  return (
+    <div className="app-bx">
+  
+      <BrowserRouter>
+      <Nav/>
+      <SignPage/>
+        <Routes>
+          <Route index Component={Home} />
+          <Route path="/profile" Component={Profile} />
+          <Route path="/prfl" >
+            <Route path="/prfl/discuss" element={<h1>Discuss</h1>} />
+            <Route path="/prfl/discuss/sub" element={<h1>Discuss-Sub</h1>} />
+          </Route>
+          <Route path="/about" Component={About} />
+        </Routes>
+      </BrowserRouter>
+      </div>
+  );
+}
+
+export default Body;
--- Components/SignPage.jsx
+++ Components/SignPage.jsx
@@ -1,60 +1,105 @@
-import React,{useState,useRef} from 'react'
-import { validate } from '../src/utils';
-
-
+import React, { useState, useRef } from "react";
+import { validate } from "../src/utils";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../src/utils/firebase";
 
 const SignPage = () => {
-
-  const [isSignIn,setisSignIn] = useState(true);
+  const [isSignIn, setisSignIn] = useState(true);
 
   const [errorMessage, seterrorMessage] = useState(null);
 
   const email = useRef(null);
   const password = useRef(null);
 
-  const handleSign=()=>{
-  
+  const handleSign = () => {
     const message = validate(email.current.value, password.current.value);
-    console.log(message)
+    console.log(message);
     seterrorMessage(message);
-  }
 
-  const handleSignIn = ()=>{
+    if (message !== null) return;
+
+    //    sign-up & sign-in logic
+    if (!isSignIn) {
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed up
+          const user = userCredential.user;
+          // ...
+          console.log(user);
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          // ..
+          seterrorMessage(errorCode + "-" + errorMessage);
+        });
+    } else {
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          // ...
+          console.log(user);
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          seterrorMessage(errorCode + "-" + errorMessage);
+        });
+    }
+  };
+
+  const handleSignIn = () => {
     return setisSignIn(!isSignIn);
-  }
-
-
-
+  };
 
   return (
-    <div className=''>
-   <div className='sign-card p-5 mx-auto'>
-
-<h2 className='text-center text-amber-100 text-3xl mb-3 sign-heading'>{isSignIn ? 'Sign In':'Sign Up'}</h2>
-<form action="" onSubmit={(e)=>e.preventDefault()}>
-{
-  !isSignIn && (<input type='text' placeholder='Enter Name'></input>)
-}
-<input ref={email} type='email' placeholder='Enter Email'></input>
-{
-  errorMessage==='Email is not Valid' && (<p className='err'>{errorMessage}</p>)
-}
-<input ref={password} type='password' placeholder='Enter Password'></input>
-{
-  errorMessage==='Password is not Valid' && (<p className='err'>{errorMessage}</p>)
-}
-<div className='text-center'>
-<button onClick={handleSign} className='mb-2.5 sign-btn'>{isSignIn?'Sign In':'Sign Up'}</button>
-
-</div>
-</form>
- <p className=' text-amber-100 new' >{isSignIn?'New to Netflix? ':'Already a User? '}
-  <span className='click-here ' onClick={handleSignIn}>{isSignIn?'Sign Up now':'Sign In now'}</span>
- </p>
-</div>
+    <div className="">
+      <div className="sign-card p-5 mx-auto">
+        <h2 className="text-center text-amber-100 text-3xl mb-3 sign-heading">
+          {isSignIn ? "Sign In" : "Sign Up"}
+        </h2>
+        <form action="" onSubmit={(e) => e.preventDefault()}>
+          {!isSignIn && <input type="text" placeholder="Enter Name"></input>}
+          <input ref={email} type="email" placeholder="Enter Email"></input>
+          {errorMessage === "Email is not Valid" && (
+            <p className="err">{errorMessage}</p>
+          )}
+          <input
+            ref={password}
+            type="password"
+            placeholder="Enter Password"
+          ></input>
+          {errorMessage === "Password is not Valid" && (
+            <p className="err">{errorMessage}</p>
+          )}
+          <p className="err">{errorMessage}</p>
+          <div className="text-center">
+            <button onClick={handleSign} className="mb-2.5 sign-btn">
+              {isSignIn ? "Sign In" : "Sign Up"}
+            </button>
+          </div>
+        </form>
+        <p className=" text-amber-100 new">
+          {isSignIn ? "New to Netflix? " : "Already a User? "}
+          <span className="click-here " onClick={handleSignIn}>
+            {isSignIn ? "Sign Up now" : "Sign In now"}
+          </span>
+        </p>
+      </div>
     </div>
- 
-  )
-}
+  );
+};
 
-export default SignPage
+export default SignPage;
